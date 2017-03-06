6f3724c660df83279bb6d9c8a8f597b0588d9f09
rfbolton <rfbolton@syr.edu>
2017-03-05 21:41:29 -0500

    added options list

--- part2.c
+++ part2.c
@@ -2,9 +2,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, cha* argv[]){
+int main(int argc, char* argv[]){
 
 
+while (opt != -1)
+	{
+		if (opt == 'h')
+		{
+			opt_h = true;
+		}
+		else if (opt == 'd')
+		{
+			opt_d = true;
+		}
+		else if (opt == 'm')
+		{
+			opt_m = true;
+		}
+		else if (opt == 't')
+		{
+			opt_t = true;
+		}
+	}
 
 
 
